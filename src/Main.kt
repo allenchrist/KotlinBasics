92ddf3d54ce73a7954b14304c8f89b95cfa08413
allenchrist <allenchristk@gmail.com>
2026-09-19 22:58:37 +0530

    Functions with Coffee Maker

--- src/Main.kt
+++ src/Main.kt
@@ -2,34 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main()
 {
-    var userinput=""
-    println("Write and option ROCK OR PAPER OR SCISSOR")
-    userinput=readln()
-
-    var choice=(1..3).random()
-
-    var computerinput=""
-
-    when(choice)
-    {
-        1->{computerinput="ROCK"}
-        2->{computerinput="PAPER"}
-        3->{computerinput="SCISSOR"}
+    println("Enter your name: ")
+    var CustomerName=readln()
+    println("Enter Number of Spoons of Sugar")
+    var Sugar=readln().toInt()
+    makeCoffee(CustomerName,Sugar)
     }
-    var winner=when
-    {
-        userinput==computerinput->"TIE"
-        userinput == "ROCK" && computerinput == "SCISSOR" -> "USER"
-        userinput == "PAPER" && computerinput == "ROCK" -> "USER"
-        userinput == "SCISSOR" && computerinput == "PAPER" -> "USER"
-        else -> "COMPUTER"
-    }
-
-    if(winner=="TIE")
-        println("GAME IS TIE")
-    else if(winner=="USER")
-        println("USER WINS")
-    else
-        println("COMPUTER WINS")
-
+fun makeCoffee(name: String,Sugar:Int)
+{
+    println("The coffer is for $name with $Sugar Spoons of Sugar")
 }
