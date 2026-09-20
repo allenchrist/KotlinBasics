44b0027849c5848f1de59d361527676850838a3e
allenchrist <allenchristk@gmail.com>
2026-09-20 12:22:38 +0530

    FOR LOOPS AND LISTS

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,59 @@
- //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main()
 {
-   //object of the class
-   var daisy=Dog("jimmy","lab",1)
+   //immutablelist
 
-   println("Summary")
-   println("Name is ${daisy.name}")
-   println("Breed is ${daisy.breed}")
-   println("Age is ${daisy.age}")
+   val numbers1=listOf(4,2,3,4,5)
 
+   //mutable list
+
+   var numbers=mutableListOf("one","Two","Three","four","five","six")
+   println(numbers)
+
+   //add methods
+
+   numbers.add("Seven")
+   numbers.add(0,"Zero")
+   println(numbers)
+
+   //size function
+
+   var sizeoflist=numbers.size
+   println(sizeoflist)
+
+   //remove methods
+
+   numbers.remove("one")
+   numbers.removeAt(0)
+   println(numbers)
+   sizeoflist=numbers.size
+   println(sizeoflist)
+
+   //contains
+
+   if(numbers.contains("one"))
+      println("There is number one in the list")
+   else
+      println("There is NO number one in the list")
+
+   //set method to modify element at particular index
+
+   numbers.set(0,"Zero")
+   println(numbers)
    println()
-   println("After one year")
-   println()
 
-   daisy.age=2 //reinilitiaze of the values in tha constructor
-   println("Name is ${daisy.name}")
-   println("Breed is ${daisy.breed}")
-   println("Age is ${daisy.age}")
+
+   //FOR LOOPS
+
+
+   for(items in numbers)
+   {
+      println(items)
+   }
+
+   //index in for lool
+   for(i in 0..numbers.size)
+      println(i)
+
+   for(i in 0 until numbers.size)
+      println(numbers[i])
 }
-
